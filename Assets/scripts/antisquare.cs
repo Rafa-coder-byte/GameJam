d22a53834b7df626e2f373931b5e9ea6164839a4
GabrielPS <gabrielperezsuarez1016@gmail.com>
2025-01-25 12:29:48 -0500

    gfgfgh

--- Assets/scripts/antisquare.cs
+++ Assets/scripts/antisquare.cs
@@ -2,70 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Antisquare : MonoBehaviour
+public class antisquare : MonoBehaviour
 {
     public GameObject cuadrado;
-    public float fadeDuration = 0.5f; // Duración del fade
-
-    private SpriteRenderer cuadradoRenderer;
-    private bool isFading = false;
-
-    void Start()
-    {
-        cuadradoRenderer = cuadrado.GetComponent<SpriteRenderer>();
-    }
-
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && !isFading)
+        if(Input.GetKeyDown(KeyCode.Space))
         {
-            StartCoroutine(FadeOut());
+            cuadrado.gameObject.SetActive(false);
         }
-
-        if (Input.GetKeyDown(KeyCode.LeftShift) && !isFading)
+        if(Input.GetKeyDown(KeyCode.B))
         {
-            Debug.Log("Activando cuadrado");
-            StartCoroutine(FadeIn());
+            
+            cuadrado.gameObject.SetActive(true);
+            ////////
+            ////////
+            ////////
         }
-    }
-
-    IEnumerator FadeOut()
-    {
-        isFading = true;
-        float startAlpha = cuadradoRenderer.color.a;
-        float rate = 1.0f / fadeDuration;
-        float progress = 0.0f;
-
-        while (progress < 1.0f)
-        {
-            Color tmpColor = cuadradoRenderer.color;
-            cuadradoRenderer.color = new Color(tmpColor.r, tmpColor.g, tmpColor.b, Mathf.Lerp(startAlpha, 0, progress));
-            progress += rate * Time.deltaTime;
-
-            yield return null;
-        }
-
-        cuadrado.SetActive(false);
-        isFading = false;
-    }
-
-    IEnumerator FadeIn()
-    {
-        cuadrado.SetActive(true);
-        isFading = true;
-        float startAlpha = cuadradoRenderer.color.a;
-        float rate = 1.0f / fadeDuration;
-        float progress = 0.0f;
-
-        while (progress < 1.0f)
-        {
-            Color tmpColor = cuadradoRenderer.color;
-            cuadradoRenderer.color = new Color(tmpColor.r, tmpColor.g, tmpColor.b, Mathf.Lerp(startAlpha, 1, progress));
-            progress += rate * Time.deltaTime;
-
-            yield return null;
-        }
-
-        isFading = false;
     }
 }
