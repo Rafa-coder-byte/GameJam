523c8426ce1e20b09e3cadb628971a743eae169a
Pablo-2205 <pablorodriguezcub@gmail.com>
2025-01-25 12:50:31 -0500

    .

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,6 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        // Detectar la tecla E para recoger objetos
         if (Input.GetKeyDown(KeyCode.E))
         {
             TryPickUpObject();
@@ -15,27 +14,33 @@ public class PlayerController : MonoBehaviour
 
     void TryPickUpObject()
     {
-        // Crear un círculo de detección alrededor del jugador
-        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, 1f); // Asegúrate de que el rango sea suficiente
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, 1f);
         foreach (var hitCollider in hitColliders)
         {
             if (hitCollider.gameObject.CompareTag("Collectible"))
             {
-                // Incrementar el puntaje o realizar la acción deseada
                 score++;
                 Debug.Log("Objeto recogido! Puntaje: " + score);
-
-                // Desactivar o destruir el objeto recogido
                 hitCollider.gameObject.SetActive(false);
             }
         }
     }
 
-    // Este método se usa solo para visualizar el rango de recogida en el editor
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Collectible"))
+        {
+            score++;
+            Debug.Log("Objeto recogido! Puntaje: " + score);
+            Destroy(other.gameObject);
+        }
+    }
+
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, 1f);
     }
 }
+
 
